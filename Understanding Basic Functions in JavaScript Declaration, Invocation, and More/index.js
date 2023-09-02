6b8dc6648e235c410fd9fdd8c260c0ef0637c8bc
Paul Theby <paultheby@gmail.com>
2023-09-02 13:53:11 -0500

    Sprint1Task- Understanding Basic Functions in Javascript

---
+++ Understanding Basic Functions in JavaScript Declaration, Invocation, and More/index.js
@@ -0,0 +1,10 @@
+//Exercises 1 then 2
+//2 Write a minimum of three distinct functions:
+///A no-parameter function displaying a greeting.
+///A function accepting a name as its parameter to show a personalized greeting.
+///A function taking two numbers as parameters, returning their sum.
+////1 
+function greeting() {
+    console.log("Hello There!")
+}
+greeting()
